e8354ff2d8c48df8b6c02b4405c01c9500dd6a77
niellesweet <dsanchezk92@gmail.com>
2017-07-14 13:15:13 -0400

    saldo.html y estilos

--- js/main.js
+++ js/main.js
@@ -30,6 +30,34 @@ $(document).ready(function(){
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
 	})
-
 })
 /*FIN PREGUNTAS FRECUENTES*/
+/*VER SALDO NO FUNCIONA A TRAVES DE JQUERY :C
+$(document).ready(function(){
+	$('#boton-ver-saldo').click(function (){
+		$('#caja-llenado').append('<div class="col-xs-10 col-xs-offset-1 text-center saldo-final" id="caja-saldo-final"><div class="row text-center saldo-total"><h4>SALDO TOTAL</h4></div><div class="row text-center valor-total"><h1 id="saldo-final-tarj"></h1></div></div>');
+
+	});	
+})
+*/
+/*API*/
+$(document).ready(function(){
+	$("#boton-ver-saldo").on("click", function(){ 
+		var saldoTotal = $("#input-tarjeta").val();
+    $.ajax({
+            url: 'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=' + saldoTotal, 
+            type: 'GET',
+            datatype: 'JSON',
+            
+        })
+
+        .done(function(response){
+        	$("#saldo-final-tarj").append("<h1 id='saldo-final-tarj'>"+ response.saldoTarjeta + "</h1>") 
+        	    console.log(response.saldoTarjeta);
+        })
+
+        .fail(function(error){
+            console.log("error");
+        })
+    });
+})
